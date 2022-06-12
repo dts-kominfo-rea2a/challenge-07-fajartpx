665386d7d3a0734c2db6c9cee4355961a7925d50
fajartpx <fajartpx@gmail.com>
2022-06-12 23:15:27 +0700

    Pembuatan Fungsi promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = async (emosi) => {
+
+  //deklarasi variable untuk memuat data hasil
+  let result = 0;
+
+  const theaterSatu = await promiseTheaterIXX();
+  const theaterDua= await promiseTheaterVGC();
+  
+  //gabungkan
+  const combineTheater = [...theaterSatu, ...theaterDua];
+ combineTheater.map((param1) => param1.hasil == emosi && (result += 1));
+  return result;
+};
 
 module.exports = {
   promiseOutput,
